c3eae247cf86a9d4e782ef7848dd99cd720a9ffd
mGolos <mathieu.golos@gmail.com>
2022-11-02 21:18:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -170,14 +170,14 @@ def contact(language="fr"):
         st.markdown('''
             # Contactez-moi
             > La meilleur façon de me joindre est par email.  
-            > Cependant, je liste ici les différents moyens de me joindre.  
+            > Cependant, je liste ici les différents moyens de me joindre.
             > N'hésitez pas à me contacter sur l'un d'entre eux !
             ''')
     elif language == "en":
         st.markdown('''
             # Contact
             > The best way to contact me is by email.  
-            > However, I list here the different methods.  
+            > However, I list here the different methods.
             > Feel free to contact me on any of them !
             ''')
         
