3a090829d015b130eab391bb7037bf0a49a55d63
mGolos <mathieu.golos@gmail.com>
2022-11-02 17:51:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -142,14 +142,14 @@ def cv(language="fr"):
                     "[Télécharger la version anglaise](https://1drv.ms/b/s!Ah3jei6FBtQIhOUsGOshMy3LKxTFVw?e=DeMrkI)")
         st.markdown(
             '''<iframe src="https://onedrive.live.com/embed?cid=08D406852E7AE31D&resid=8D406852E7AE31D%2178505&authkey=ACciPtwGdV60lf4&em=2"*
-            width="100%" height="1060" frameborder="0" scrolling="no"></iframe>''',
+            width="100%" height="142%" frameborder="0" scrolling="no"></iframe>''',
             unsafe_allow_html =True)
     elif language == "en":
         st.markdown("[Download the english version](https://1drv.ms/b/s!Ah3jei6FBtQIhOUsGOshMy3LKxTFVw?e=DeMrkI) | "
                     "[Download the french version](https://1drv.ms/b/s!Ah3jei6FBtQIhOUpb7Wx-3nUGQrTWQ?e=5qYyve)")
         st.markdown(
             '''<iframe src="https://onedrive.live.com/embed?cid=08D406852E7AE31D&resid=8D406852E7AE31D%2178508&authkey=APB7Ex6E01QEURo&em=2"*
-            width="100%" height="1060" frameborder="0" scrolling="no"></iframe>''',
+            width="100%" height="200%" frameborder="0" scrolling="no"></iframe>''',
             unsafe_allow_html =True)
     
     
