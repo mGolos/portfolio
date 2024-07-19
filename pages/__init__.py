53b2d9e6a12a53eee5e7d3b57891854e0792b2de
mathieugolos <mathieu.golos@lindera.de>
2024-07-19 18:06:31 +0200

    fix dropping timeline

--- pages/__init__.py
+++ pages/__init__.py
@@ -2,7 +2,6 @@ from . import about
 from . import journey
 from . import projects
 from . import skills
-from . import timeline
 from . import publications
 from . import cv
 from . import recommendations
