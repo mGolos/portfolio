389b92104cd536843e6d170ef7c4fe270e55a41a
mathieugolos <mathieu.golos@lindera.de>
2024-07-19 18:02:36 +0200

    droping timeline

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     utils.always()
     
     pages = [
-        'about', 'journey', 'projects', 
-        'skills', 'timeline',
+        'about', 'journey', 'projects', 'skills', 
         'publications', 'cv', 'recommendations']
     for page in pages:
         submodule = importlib.import_module(f'pages.{page}')
