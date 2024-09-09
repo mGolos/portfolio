891dad95697132b0ed9ae1f0d818862183d5bc18
mathieugolos <mathieu.golos@lindera.de>
2024-09-09 22:03:42 +0200

    recommandation letter not changing over time

--- cpages/recommendations.py
+++ cpages/recommendations.py
@@ -5,25 +5,13 @@ sss = st.session_state
 
 def main():
     lang = sss['language']
-    filepath1 = f"rl1_{lang}.pdf"
-    filepath2 = f"rl2_{lang}.pdf"
-    down_url = "https://onedrive.live.com/download?{id}&ithint=file%2cpdf"
+    filepath1 = f"images/rl1_{lang}.pdf"
+    filepath2 = f"images/rl2_{lang}.pdf"
     
-    match lang:
-        case "en":
-            id1 = "resid=8D406852E7AE31D%2179528&authkey=%21AKf1P7B3SVNyOho"
-            id2 = "resid=8D406852E7AE31D%2149593&authkey=AGqw88JCpvqX2_Q"
-            st.header("Recommendations", anchor='recommendations', divider="orange")
-        case "fr":
-            id1 = "resid=8D406852E7AE31D%2179527&authkey=%21ADJYWIi%5FeQVejV4"
-            id2 = "resid=8D406852E7AE31D%2178515&authkey=AD4gq0CU3sAJEnk"
-            st.header("Recommandations", anchor='recommendations', divider="orange")
-
+    st.header("Recommandations" if sss["lg_key"] else "Recommendations", anchor='recommendations', divider="orange")
     tab1, tab2 = st.tabs(["XRator", "INS"])
-    image1 = utils.download_pdf(down_url.format(id=id1), filepath1)
-    image2 = utils.download_pdf(down_url.format(id=id2), filepath2)
-    tab1.image(image1)
-    tab2.image(image2)
+    tab1.image(filepath1)
+    tab2.image(filepath2)
 
 
 if __name__ == "__main__":
