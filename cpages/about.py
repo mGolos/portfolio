95888b56c418d7f5ace06c6478569621c21b6e04
mathieugolos <mathieu.golos@lindera.de>
2024-09-10 17:32:56 +0200

    about

--- cpages/about.py
+++ cpages/about.py
@@ -10,13 +10,13 @@ def main():
             > Bonjour, je m'appelle Mathieu Golos.  
             
             > Polyvalent avec un profil orienté vers la **science** et la **programmation**, je suis un travailleur altruiste et créatif.
-            > J'aime mon autonomie mais je m'épanouis au sein d'une équipe avec laquelle il est primordial pour moi de créer des liens.  
+            > J'ai toujours eu un vif intérêt pour les disciplines scientifiques et l'ai satisfait par une **formation pluridisciplinaire**.
+            > Une thèse enrichissante en **neurosciences computationnelles** m'a amené à travailler sur l'**Apprentissage Automatique** et les **Séries temporelles**.  
             
-            > J'ai toujours eu un vif intérêt pour les disciplines scientifiques et l'ai satisfait par une formation pluridisciplinaire qui m'a conduit à un master spécialisé en Modélisation et Calcul Scientifiques.
-            > Formé au calcul intensif, j'ai effectué une thèse enrichissante en neurosciences computationnelles, travaillant sur les réseaux de neurones, l'apprentissage automatique et les séries temporelles.  
+            > J'ai pu m'investir par la suite dans la **cyber prévention**, le **biomédical** ou le **bien-être canin**, et je cherche toujours de nouveaux projets captivants.
+            > **Autonome** mais je m'épanouis au sein d'une équipe avec laquelle il est primordial pour moi de **créer des liens**.  
             
-            > Après un stage en tant qu'ingénieur Machine Learning, j'ai travaillé comme Expert en Science des données chez XRator qui aborde les risques cyber de manière préventive, et chez Jagger&Lewis pour prédire les comportements de chiens à partir d'activités temporelles.
-            > Je cherche actuellement à en apprendre davantage dans certains domaines au cours de la prochaine décennie pour un projet à long terme.
+            > Ouvert à toute proposition et tout type de contrat.
         """)
     
     elif sss['language'] == "en":
@@ -24,14 +24,14 @@ def main():
         st.markdown("""
             > Hello, my name is Mathieu Golos.  
             
-            > Versatile with a profile oriented towards science and programming, I am an altruistic and creative worker.
-            > I like my autonomy but I flourish within a team with which it is essential for me to create links.  
+            > Versatile with a profile oriented towards **science** and **programming**, I am an altruistic and creative worker.
+            > I always had a keen interest in scientific disciplines and satisfied this with **multidisciplinary training**.
+            > An enriching thesis in **Computational Neurosciences**, led me working on **Machine Learning** and **Time Series**.  
             
-            > I always had a keen interest in scientific disciplines and satisfied this with multidisciplinary training which led me to a master's degree specializing in Scientific Modeling and Computing.
-            > Trained in Intensive Computing, I did an enriching thesis in Computational Neurosciences, working on Neural Networks, Machine Learning and Time Series.  
-
-            > After an internship as a Machine Learning engineer, I worked as a Data Scientist at XRator which approaches cyber risks in a preventive way, and Jagger&Lewis to predict dogs' behaviors from temporal activities.
-            > I am currently looking to learn more in certain areas over the next decade for a long-term project.
+            > I could invest myself in **cyber prevention**, **biomedical**, or **canine well-being**, and I'm always looking for new captivating projects.
+            > **Independent**, but I thrive within a team with whom it's essential for me to **build relationships**.
+            
+            > Open to all proposals and any type of contract.
         """)
     
     st.image('images/photo1.jpg')
